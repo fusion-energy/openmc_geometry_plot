088ce5211f1620c5bd2d050967329ada2f1ca20a
Jonathan Shimwell <mail@jshimwell.com>
2022-12-05 11:36:56 +0000

    added color map to mpl

--- examples/tokamak.py
+++ examples/tokamak.py
@@ -1,65 +1,115 @@
+# plots a geometry with both backends (matplotlib and plotly)
+# A couple of zoomed in plots are plotted
+# All view angles (x, y, z) for the whole geometry are plotted
 
 
 import openmc
 from openmc_geometry_plot import plot_axis_slice
 
 
-# surfaces
-central_column_surface = openmc.ZCylinder(r=100) # note the new surface type
-inner_sphere_surface = openmc.Sphere(r=480)
-middle_sphere_surface = openmc.Sphere(r=500) 
-outer_sphere_surface = openmc.Sphere(r=600, boundary_type='vacuum')
+breeder_material = openmc.Material()
+breeder_material.add_element("Li", 1.0)
+breeder_material.set_density("g/cm3", 0.01)
 
-# regions
-# the center column region is cut at the top and bottom using the -outer_sphere_surface
-central_column_region = -central_column_surface & -outer_sphere_surface
-firstwall_region = -middle_sphere_surface & +inner_sphere_surface & +central_column_surface
-blanket_region = +middle_sphere_surface & -outer_sphere_surface & +central_column_surface
-inner_vessel_region = +central_column_surface & -inner_sphere_surface
+copper_material = openmc.Material()
+copper_material.set_density("g/cm3", 0.01)
+copper_material.add_element("Li", 1.0)
+
+eurofer_material = openmc.Material()
+eurofer_material.set_density("g/cm3", 0.01)
+eurofer_material.add_element("Li", 1)
+
+# surfaces
+central_sol_surface = openmc.ZCylinder(r=100)
+central_shield_outer_surface = openmc.ZCylinder(r=110)
+# plasma_surface = openmc.ZTorus(x0=200,y0=200)
+port_hole = openmc.Sphere(r=60, x0=500)
+upper_port_hole = openmc.Sphere(r=100, z0=500)
+vessel_inner_surface = openmc.Sphere(r=500)
+first_wall_outer_surface = openmc.Sphere(r=510)
+breeder_blanket_outer_surface = openmc.Sphere(r=610, boundary_type="vacuum")
 
 # cells
-firstwall_cell = openmc.Cell(region=firstwall_region)
-central_column_cell = openmc.Cell(region=central_column_region)
-blanket_cell = openmc.Cell(region=blanket_region)
+central_sol_region = (
+    -central_sol_surface & -breeder_blanket_outer_surface & +upper_port_hole
+)
+central_sol_cell = openmc.Cell(region=central_sol_region)
+central_sol_cell.fill = copper_material
+
+central_shield_region = (
+    +central_sol_surface
+    & -central_shield_outer_surface
+    & -breeder_blanket_outer_surface
+    & +upper_port_hole
+)
+central_shield_cell = openmc.Cell(region=central_shield_region)
+central_shield_cell.fill = eurofer_material
+
+inner_vessel_region = (
+    -vessel_inner_surface
+    & +central_shield_outer_surface
+    & +port_hole
+    & +upper_port_hole
+)
 inner_vessel_cell = openmc.Cell(region=inner_vessel_region)
+# no material set as default is vacuum
 
-universe = openmc.Universe(cells=[central_column_cell, firstwall_cell,
-                                  blanket_cell, inner_vessel_cell])
+upper_port_hole_region = -upper_port_hole
+upper_port_hole_cell = openmc.Cell(region=upper_port_hole_region)
 
+port_hole_region = -port_hole
+port_hole_cell = openmc.Cell(region=port_hole_region)
+# no material set as default is vacuum
+
+first_wall_region = (
+    -first_wall_outer_surface & +vessel_inner_surface & +port_hole & +upper_port_hole
+)
+first_wall_cell = openmc.Cell(region=first_wall_region)
+first_wall_cell.fill = eurofer_material
+
+breeder_blanket_region = (
+    +first_wall_outer_surface
+    & -breeder_blanket_outer_surface
+    & +central_shield_outer_surface
+    & +port_hole
+    & +upper_port_hole
+)
+breeder_blanket_cell = openmc.Cell(region=breeder_blanket_region)
+breeder_blanket_cell.fill = breeder_material
+
+universe = openmc.Universe(
+    cells=[
+        central_sol_cell,
+        central_shield_cell,
+        inner_vessel_cell,
+        first_wall_cell,
+        breeder_blanket_cell,
+        port_hole_cell,
+        upper_port_hole_cell,
+    ]
+)
 my_geometry = openmc.Geometry(universe)
 
 
-# plots the geometry
-plot = plot_axis_slice(
-    geometry=my_geometry,
-    view_direction='z',
-    backend='matplotlib'
-)
+for backend in ["matplotlib", "plotly"]:
 
-plot.show()
+    for view_direction in ["z", "x", "y"]:
+        # plots the geometry
+        plot = plot_axis_slice(
+            geometry=my_geometry, view_direction=view_direction, backend=backend
+        )
 
-# plots the geometry
-plot = plot_axis_slice(
-    geometry=my_geometry,
-    view_direction='x',
-    backend='matplotlib'
-)
-plot.show()
+        plot.show()
 
-plot = plot_axis_slice(
-    geometry=my_geometry,
-    view_direction='y',
-    backend='matplotlib'
-)
+    # a single zoomed in plot
+    plot = plot_axis_slice(
+        geometry=my_geometry,
+        plot_left=400,
+        plot_right=600,
+        plot_top=-200,
+        plot_bottom=100,
+        view_direction="y",
+        backend=backend,
+    )
 
-plot = plot_axis_slice(
-    geometry=my_geometry,
-    plot_left=400,
-    plot_right=600,
-    plot_top=-200,
-    plot_bottom=100,
-    view_direction='y',
-    backend='matplotlib'
-)
-
-plot.show()
+    plot.show()
--- src/openmc_geometry_plot/core.py
+++ src/openmc_geometry_plot/core.py
@@ -6,49 +6,50 @@ import plotly.graph_objects as go
 
 def check_for_inf_value(var_name):
     if np.isinf(var_name):
-        msg=f"{var_name} can't be obtained from the bounding box as boundary is at inf. {var_name} value must be specified by user"
+        msg = f"{var_name} can't be obtained from the bounding box as boundary is at inf. {var_name} value must be specified by user"
         raise ValueError(msg)
+
 
 def plot_axis_slice(
     geometry,
-    view_direction='x',
+    view_direction="x",
     plot_left=None,
     plot_right=None,
     plot_top=None,
     plot_bottom=None,
     slice_value=None,
-    pixels_across=200,
-    backend='plotly'
+    pixels_across=500,
+    backend="plotly",
+    title=None,
 ):
 
     bb = geometry.bounding_box
 
     if view_direction == "x":
         # need plot_left, plot_right, plot_top, plot_bottom
-    
+
         if plot_left is None:
             plot_left = bb[0][1]
             check_for_inf_value(plot_left)
-    
+
         if plot_right is None:
             plot_right = bb[1][1]
             check_for_inf_value(plot_right)
-    
+
         if plot_top is None:
             plot_top = bb[1][2]
             check_for_inf_value(plot_top)
-    
+
         if plot_bottom is None:
             plot_bottom = bb[0][2]
             check_for_inf_value(plot_bottom)
 
         if slice_value is None:
-            slice_value = (bb[0][0] + bb[1][0])/2
+            slice_value = (bb[0][0] + bb[1][0]) / 2
             check_for_inf_value(slice_value)
 
         xlabel = "Y [cm]"
         ylabel = "Z [cm]"
-
 
     if view_direction == "y":
         # need plot_left, plot_right, plot_top, plot_bottom
@@ -56,58 +57,57 @@ def plot_axis_slice(
         if plot_left is None:
             plot_left = bb[0][0]
             check_for_inf_value(plot_left)
-    
+
         if plot_right is None:
             plot_right = bb[1][0]
             check_for_inf_value(plot_right)
-    
+
         if plot_top is None:
             plot_top = bb[1][2]
             check_for_inf_value(plot_top)
-    
+
         if plot_bottom is None:
             plot_bottom = bb[0][2]
             check_for_inf_value(plot_bottom)
 
         if slice_value is None:
-            slice_value = (bb[0][1] + bb[1][1])/2
+            slice_value = (bb[0][1] + bb[1][1]) / 2
             check_for_inf_value(slice_value)
 
         xlabel = "X [cm]"
         ylabel = "Z [cm]"
 
-
     if view_direction == "z":
         # need plot_left, plot_right, plot_top, plot_bottom
-    
+
         if plot_left is None:
             plot_left = bb[0][0]
             check_for_inf_value(plot_left)
-    
+
         if plot_right is None:
             plot_right = bb[1][0]
             check_for_inf_value(plot_right)
-    
+
         if plot_top is None:
             plot_top = bb[1][1]
             check_for_inf_value(plot_top)
-            print('plot_top',plot_top)
-    
+            print("plot_top", plot_top)
+
         if plot_bottom is None:
             plot_bottom = bb[0][1]
             check_for_inf_value(plot_bottom)
-            print('plot_bottom',plot_bottom)
+            print("plot_bottom", plot_bottom)
 
         if slice_value is None:
-            slice_value = (bb[0][2] + bb[1][2])/2
+            slice_value = (bb[0][2] + bb[1][2]) / 2
             check_for_inf_value(slice_value)
 
         xlabel = "X [cm]"
         ylabel = "Y [cm]"
 
-    plot_width = abs(plot_left-plot_right)
-    plot_height = abs(plot_bottom-plot_top)
-    print('plot_height',plot_height)
+    plot_width = abs(plot_left - plot_right)
+    plot_height = abs(plot_bottom - plot_top)
+    print("plot_height", plot_height)
 
     aspect_ratio = plot_height / plot_width
     pixels_up = int(pixels_across * aspect_ratio)
@@ -117,7 +117,7 @@ def plot_axis_slice(
     for plot_y in np.linspace(plot_top, plot_bottom, pixels_up):
         row_cell_ids = []
         for plot_x in np.linspace(plot_left, plot_right, pixels_across):
-            
+
             if view_direction == "z":
                 found = geometry.find((plot_x, plot_y, slice_value))
             if view_direction == "x":
@@ -132,54 +132,84 @@ def plot_axis_slice(
                     mat = found[1].fill
                     material_ids.append(str(mat.id))
                 else:
-                    material_ids.append('void')
+                    material_ids.append("void")
             else:
                 row_cell_ids.append(0)
-                material_ids.append('void')
+                material_ids.append("void")
         cell_ids.append(row_cell_ids)
 
-    # print(material_ids)
-    if backend == 'matplotlib':
+    if title is None:
+        # consider adding a link, does not work well in mpl
+        # title = 'Made with <a href="https://github.com/fusion-energy/openmc_geometry_plot/">openmc-geometry-plot</a>'
+        title = (
+            f"Slice through OpenMC geometry with view direction of {view_direction}"
+        )
 
-        plot = plt.imshow(cell_ids, extent=(plot_left, plot_right, plot_bottom, plot_top))
+    if backend == "matplotlib":
+
+        from matplotlib import colors
+        cmap = plt.cm.jet
+        cmaplist = [cmap(i) for i in range(cmap.N)]
+        cmaplist[0] = (1., 1., 1., 0.)
+        cmap = colors.LinearSegmentedColormap.from_list('Custom cmap', cmaplist, cmap.N)
+        bounds = list(geometry.get_all_cells().keys())  + [0]
+        print(bounds)
+        bounds.sort()
+        print(bounds)
+        norm = colors.BoundaryNorm(bounds, cmap.N)
+
+        plot = plt.imshow(
+            cell_ids,
+            extent=(plot_left, plot_right, plot_bottom, plot_top),
+            interpolation='none',
+            # origin='lower', # this flips the axis incorrectly
+            cmap=cmap,
+            norm=norm,
+        )
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
+        plt.title(title)
         return plt
- 
-    elif backend=='plotly':
+
+    elif backend == "plotly":
 
         plot = go.Figure(
             data=go.Heatmap(
                 z=cell_ids,
-                colorscale='viridis',
-                x0 =plot_left,
-                dx=abs(plot_left-plot_right)/(len(cell_ids[0])-1),
-                y0 =plot_bottom,
-                dy=abs(plot_bottom-plot_top)/(len(cell_ids)-1),
+                colorscale="viridis",
+                x0=plot_left,
+                dx=abs(plot_left - plot_right) / (len(cell_ids[0]) - 1),
+                y0=plot_bottom,
+                dy=abs(plot_bottom - plot_top) / (len(cell_ids) - 1),
                 # colorbar=dict(title=dict(side="right", text=cbar_label)),
                 # text = material_ids,
-                hovertemplate =
-                    # 'material ID = %{z}<br>'+
-                    'Cell ID = %{z}<br>'+
-                    # '<br>%{text}<br>'+
-                    xlabel[:2].title()+': %{x} cm<br>'+
-                    ylabel[:2].title()+': %{y} cm<br>'                            
-                ),
-            )
-        
+                hovertemplate=
+                # 'material ID = %{z}<br>'+
+                "Cell ID = %{z}<br>" +
+                # '<br>%{text}<br>'+
+                xlabel[:2].title()
+                + ": %{x} cm<br>"
+                + ylabel[:2].title()
+                + ": %{y} cm<br>",
+            ),
+        )
 
         plot.update_layout(
             xaxis={"title": xlabel},
-            yaxis={"title": ylabel},
+            # reversed autorange is required to avoid image needing rotation/flipping in plotly
+            yaxis={"title": ylabel, "autorange": "reversed"},
+            title=title,
             autosize=False,
             height=800,
         )
         plot.update_yaxes(
-            scaleanchor = "x",
-            scaleratio = 1,
+            scaleanchor="x",
+            scaleratio=1,
         )
 
     else:
-        raise ValueError(f"Supported backend are 'plotly' and 'matplotlib', not {backend}")
-        
+        raise ValueError(
+            f"Supported backend are 'plotly' and 'matplotlib', not {backend}"
+        )
+
     return plot
