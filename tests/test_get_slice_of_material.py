ca54ad725b01bee2258abf604bfc1b8afa2a5c8e
Jonathan Shimwell <mail@jshimwell.com>
2023-02-13 16:13:00 +0000

    view direction now arg

--- tests/test_get_slice_of_material.py
+++ tests/test_get_slice_of_material.py
@@ -1,5 +1,6 @@
 import openmc
 import openmc_geometry_plot
+import numpy as np
 
 def test_get_slice_of_material_ids():
     
@@ -18,7 +19,9 @@ def test_get_slice_of_material_ids():
     
     my_geometry = openmc.Geometry(cells)
     
-    slice_data = my_geometry.get_slice_of_material_ids()
-    
-    my_geometry.view_direction = 'x'
-    assert slice_data.shape == (500, 500)
+    slice_data = my_geometry.get_slice_of_material_ids(view_direction='x')
+
+    assert np.array(slice_data).shape == (500, 500)
+    for rows in slice_data:
+        for value in rows:
+            assert value in [1, 0]
