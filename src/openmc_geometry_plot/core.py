ba1bdd6cd4dd59e0aba88ad7c7d39f9b43f15459
Jonathan Shimwell <mail@jshimwell.com>
2023-05-18 12:29:25 +0100

    setting cross sections in edge case

--- src/openmc_geometry_plot/core.py
+++ src/openmc_geometry_plot/core.py
@@ -478,7 +478,10 @@ def get_slice_of_cell_ids(
     my_plots = openmc.Plots([my_plot])
     my_plots.export_to_xml(tmp_folder)
 
-    original_cross_sections = openmc.config["cross_sections"]
+    if 'cross_sections' in openmc.config.keys():
+        original_cross_sections = openmc.config["cross_sections"]
+    else:
+        original_cross_sections = None
 
     # TODO unset this afterwards
     package_dir = Path(__file__).parent
@@ -486,7 +489,8 @@ def get_slice_of_cell_ids(
 
     openmc.plot_geometry(cwd=tmp_folder, output=verbose)
 
-    openmc.config["cross_sections"] = original_cross_sections
+    if original_cross_sections:
+        openmc.config["cross_sections"] = original_cross_sections
 
     if verbose:
         print(f"Temporary image and xml files written to {tmp_folder}")
